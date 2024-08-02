9b2565822b8447338d4b54e5801bcbf12056904e
Renato Aguilar <renato.aguilar@repn.edu.ec>
2024-08-01 21:35:04 -0500

    Creacion de la funcion eliminar cliente

--- IngresoClientes.cpp
+++ IngresoClientes.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
 using namespace std;
 
 int imprimir_opciones(){
@@ -18,7 +17,8 @@ int imprimir_opciones(){
 //ingreso de cliente
 void ingreso_cliente(){
     string nombres, apellidos;
-    long long int cedula;
+    long long int cedula, numero_tarjeta;
+    int cvc;
     ofstream archivo("CRUD.csv",ios::app);
     if(archivo.is_open()){
         cin.ignore();
@@ -32,10 +32,16 @@ void ingreso_cliente(){
         cout<<"Número de cedula incorrecto, ingrese nuevamente: ";
         cin>>cedula;
         cout<<endl;
-    }
-        archivo<<"Nombres: "<<";"<<nombres<<endl; 
-        archivo<<"Apellidos: "<<";"<<apellidos<<endl, 
-        archivo<<"Cedula: "<<";"<<cedula<<endl;
+        }
+        cout<<"Ingrese el número de la tarjeta: ";
+        cin>>numero_tarjeta;
+        cout<<"Ingrese el CVC de la tarjeta: ";
+        cin>>cvc;
+        while (cvc>=1000 || cvc<100){
+            cout<<"El CVC debe tener al menos tres dígitos, intentelo de nuevo: ";
+            cin>>cvc;
+        }
+        archivo<<nombres<<";"<<apellidos<<";"<<cedula<<";"<<numero_tarjeta<<";"<<cvc<<endl;
         archivo.close();
     }else{
         cout << "Error al abrir el archivo" << endl;
@@ -52,10 +58,43 @@ void crear_archivo(){
     }else{
         cout << "Error al crear el archivo" << endl;
         exit(1);
+  }
+}
+
+//Eliminar Datos
+void eliminar_cliente(){
+    long long int cedula;
+    cout<<"Ingrese la cedula del cliente a eliminar: ";
+    cin>>cedula;
+    while(cedula<=100000000||cedula>=2500000000){
+        cout<<"El número de cedula debe tener diez dígitos, ingrese nuevamente: ";
+        cin>>cedula;
+        cout<<endl;
+    }
+    ifstream archivo("CRUD.csv");
+    ofstream archivo_temp("temp.csv");
+
+    if(archivo.is_open() && archivo_temp.is_open()){
+        string linea;
+        while(getline(archivo, linea)){
+            if(linea.find(to_string(cedula))==string::npos){
+                archivo_temp<<linea<<endl;
+            }
+        }
+        archivo.close();
+        archivo_temp.close();
+        remove("CRUD.csv");
+        rename("temp.csv","CRUD.csv");
+        cout<<"Datos del cliente eliminados con exito "<<endl;
+    }else{
+        cout<<"Error al abrir los archivos"<<endl;
+        exit(1);
     }
 }
 
+
 int main(){
+    setlocale(LC_ALL,"spanish");
     int n;
     crear_archivo();
     do
@@ -65,6 +104,9 @@ int main(){
         {
         case 1: //ingreso de clientes
             ingreso_cliente();
+            break;
+        case 4:
+            eliminar_cliente();
             break;
         case 5:
             cout<<"Gracias por usar el programa"<<endl;
